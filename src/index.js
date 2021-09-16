97fc2efb47e9e427c90b576d4f6d54aa56dc06d0
tian <tianwu@outlook.com>
2021-09-16 10:27:58 +1000

    add chapter 3 - basics of node & express

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 4001;
+
+app.get('/', (req, res) => res.send('Hello there!!!'));
+
+app.listen(4001, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
